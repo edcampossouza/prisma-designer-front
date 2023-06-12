b2187e6acd1567bb86ecb800da1f562b40746fee
Eduardo Souza <edcampossouza@gmail.com>
2023-06-12 15:05:24 -0300

    feat: show command options

--- src/app/components/MainMenu.tsx
+++ src/app/components/MainMenu.tsx
@@ -11,18 +11,28 @@ type Props = {
 };
 export default function MainMenu(props: Props) {
   return (
-    <menu className="w-full h-16 bg-orange-300 flex justify-center items-center">
-      <IconButton onClick={props.createEntity}>
-        <AiFillPlusCircle />
-      </IconButton>
-      <AiFillPlusCircle
-        onClick={() => props.createField()}
-        className="bg-red-400 rounded-md text-white text-lg hover:cursor-pointer w-12"
-      />
-      <AiOutlineSend
-        onClick={() => props.generateSchema()}
-        className="bg-red-400 rounded-md text-white text-lg hover:cursor-pointer w-12"
-      />
+    <menu className="w-full h-16 bg-orange-300 flex justify-around items-center">
+      <div className="  flex flex-col items-center">
+        Create [M]odel
+        <AiFillPlusCircle
+          onClick={() => props.createEntity()}
+          className="bg-blue-400 rounded-md text-white text-lg hover:cursor-pointer w-12"
+        />
+      </div>
+      <div className="  flex flex-col items-center">
+        Create [F]ield
+        <AiFillPlusCircle
+          onClick={() => props.createField()}
+          className="bg-red-400 rounded-md text-white text-lg hover:cursor-pointer w-12"
+        />
+      </div>
+      <div className="  flex flex-col items-center">
+        Generate
+        <AiOutlineSend
+          onClick={() => props.generateSchema()}
+          className="bg-green-400 rounded-md text-white text-lg hover:cursor-pointer w-12"
+        />
+      </div>
     </menu>
   );
 }
