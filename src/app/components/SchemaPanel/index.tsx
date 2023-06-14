ba8ade2876e50eefe914763c607c0ed6c7d7506b
Eduardo Souza <edcampossouza@gmail.com>
2023-06-14 11:18:36 -0300

    feat: copy button

--- src/app/components/SchemaPanel/index.tsx
+++ src/app/components/SchemaPanel/index.tsx
@@ -1,6 +1,7 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { Schema, SerializedSchema } from "prismadesign-lib";
+import { FaRegCopy } from "react-icons/fa";
 
 type Props = {
   schema: Schema;
@@ -30,9 +31,31 @@ function serializeSchema(schema: SerializedSchema) {
 
 export default function SchemaPanel(props: Props) {
   const { formattedText, schema } = props;
+  const [copyMsg, setCopyMsg] = useState(false);
   return (
-    <div className="fixed bottom-0 z-50 w-full bg-blue-500  whitespace-pre-wrap">
-      {formattedText ? formattedText : serializeSchema(schema.toSerial())}
+    <div className="fixed w-full bottom-0 ">
+      <div className="relative z-50 w-full bg-blue-500  whitespace-pre-wrap font-mono">
+        {formattedText
+          ? formattedText
+          : `Click "Generate" to generate .prisma file`}
+        <div className=" text-white absolute top-2 right-2 flex flex-col items-end">
+          <FaRegCopy
+            className="hover:cursor-pointer "
+            onClick={() => {
+              if (formattedText) {
+                navigator.clipboard.writeText(formattedText);
+                setCopyMsg(true);
+                setTimeout(() => {
+                  setCopyMsg(false);
+                }, 2000);
+              }
+            }}
+          />
+          <span className={`text text-xs ${copyMsg ? "" : "hidden"} `}>
+            Copied to clipboard
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
