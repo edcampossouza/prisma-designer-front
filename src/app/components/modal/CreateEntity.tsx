c5078404e5b19ae12d78cceb64f3e78b8ad41e7c
Eduardo Souza <edcampossouza@gmail.com>
2023-06-14 14:51:28 -0300

    feat: create model: default id field

--- src/app/components/modal/CreateEntity.tsx
+++ src/app/components/modal/CreateEntity.tsx
@@ -4,15 +4,16 @@ import React, { useState, useRef, useEffect } from "react";
 
 type Props = {
   hidden: boolean;
-  submit?: Function;
+  submit?: (name: string, createId?: boolean) => void;
 };
 
 export default function CreateEntity(props: Props) {
   const inputRef = useRef<HTMLInputElement>(null);
   const [name, setName] = useState("");
+  const [toCreateId, setToCreateId] = useState(true);
   function submit() {
     if (props.submit) {
-      props.submit(name);
+      props.submit(name, toCreateId);
       setName("");
     }
   }
@@ -29,18 +30,29 @@ export default function CreateEntity(props: Props) {
         props.hidden && "invisible"
       }`}
     >
-      Model name
-      <input
-        ref={inputRef}
-        className="text-lg bg-slate-200"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        onKeyDown={(event: React.KeyboardEvent<HTMLInputElement>) => {
-          if (event.key === "Enter") {
-            submit();
-          }
-        }}
-      />
+      <label>
+        Model name
+        <input
+          ref={inputRef}
+          className="text-lg bg-slate-200"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          onKeyDown={(event: React.KeyboardEvent<HTMLInputElement>) => {
+            if (event.key === "Enter") {
+              submit();
+            }
+          }}
+        />
+      </label>
+      <label>
+        Create id field?
+        <input
+          className=""
+          type="checkbox"
+          checked={toCreateId}
+          onChange={(e) => setToCreateId(e.target.checked)}
+        />
+      </label>
       <button
         className="rounded bg-orange-300 hover:bg-orange-500 w-fit mx-auto px-2 "
         onClick={submit}
