669265d007da015f67206a06cc91dce84f8604ff
Eduardo Souza <edcampossouza@gmail.com>
2023-06-19 09:54:03 -0300

    feat: sign in from main screen

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,85 +1,27 @@
 "use client";
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
-import axios from "axios";
-import { signIn } from "../services/auth";
-import { storeUserInfo } from "../util/auth";
+import SignInComponent from "./SignInComponent";
 
 export default function SignUp() {
   const router = useRouter();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [infoMessage, setInfoMessage] = useState("");
 
-  async function submit(e: React.FormEvent) {
-    e.preventDefault();
-    setInfoMessage("");
-    try {
-      const response = await signIn(email, password);
-      storeUserInfo(response.data);
-    } catch (error) {
-      if (axios.isAxiosError(error)) {
-        setInfoMessage(error.response?.data?.message || error.message);
-      }
-    }
+  function onSignUpLink() {
+    router.push("/sign-up");
+  }
+
+  function onSuccess() {
+    router.push("/");
   }
 
   return (
     <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
-      <div className="w-full p-6 m-auto bg-white rounded-md shadow-xl shadow-rose-600/40 ring-2 ring-purple-600 lg:max-w-xl">
-        <h1 className="text-3xl font-semibold text-center text-purple-700 uppercase">
-          Sign in
-        </h1>
-        <form className="mt-6" onSubmit={submit}>
-          <div className="mb-2">
-            <label className="block text-sm font-semibold text-gray-800">
-              Email
-            </label>
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-            />
-          </div>
-          <div className="mb-2">
-            <label className="block text-sm font-semibold text-gray-800">
-              Password
-            </label>
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-            />
-          </div>
-          {infoMessage && (
-            <div className="mb-2">
-              <span className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40">
-                {infoMessage}
-              </span>
-            </div>
-          )}
-          <div className="mt-6">
-            <button
-              type="submit"
-              className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600"
-            >
-              Login
-            </button>
-          </div>
-        </form>
-
-        <p className="mt-8 text-xs font-light text-center text-gray-700">
-          Don't have an account yet?
-          <a
-            href="/sign-up"
-            className="font-medium text-purple-600 hover:underline"
-          >
-            Sign up
-          </a>
-        </p>
-      </div>
+      <SignInComponent
+        onFailure={() => {}}
+        onSuccess={onSuccess}
+        onSignUpLink={onSignUpLink}
+      />
+      ;
     </div>
   );
 }
--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -3,102 +3,21 @@ import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
 import { signUp } from "../services/auth";
+import SignUpComponent from "./SignUpComponent";
 
 export default function SignUp() {
   const router = useRouter();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [infoMessage, setInfoMessage] = useState("");
-
-  async function submit(e: React.FormEvent) {
-    e.preventDefault();
-    setInfoMessage("");
-    if (password !== confirmPassword) {
-      setInfoMessage("Passwords do not match");
-      setTimeout(() => {
-        setInfoMessage("");
-      }, 3000);
-      return;
-    } else {
-      try {
-        await signUp(email, password);
-        router.push("/sign-in");
-      } catch (error) {
-        if (axios.isAxiosError(error)) {
-          setInfoMessage(error.response?.data?.message || error.message);
-        }
-      }
-    }
+  function goToSignIn() {
+    router.push("/sign-in");
   }
 
   return (
     <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
-      <div className="w-full p-6 m-auto bg-white rounded-md shadow-xl shadow-rose-600/40 ring-2 ring-purple-600 lg:max-w-xl">
-        <h1 className="text-3xl font-semibold text-center text-purple-700 uppercase">
-          Sign in
-        </h1>
-        <form className="mt-6" onSubmit={submit}>
-          <div className="mb-2">
-            <label className="block text-sm font-semibold text-gray-800">
-              Email
-            </label>
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-            />
-          </div>
-          <div className="mb-2">
-            <label className="block text-sm font-semibold text-gray-800">
-              Password
-            </label>
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-            />
-          </div>
-          <div className="mb-2">
-            <label className="block text-sm font-semibold text-gray-800">
-              Confirm Password
-            </label>
-            <input
-              type="password"
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-              className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-            />
-          </div>
-          {infoMessage && (
-            <div className="mb-2">
-              <span className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40">
-                {infoMessage}
-              </span>
-            </div>
-          )}
-          <div className="mt-6">
-            <button
-              type="submit"
-              className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600"
-            >
-              Sign Up
-            </button>
-          </div>
-        </form>
-
-        <p className="mt-8 text-xs font-light text-center text-gray-700">
-          Already have an account?
-          <a
-            href="/sign-in"
-            className="font-medium text-purple-600 hover:underline"
-          >
-            Sign in
-          </a>
-        </p>
-      </div>
+      <SignUpComponent
+        onSuccess={goToSignIn}
+        onFailure={() => {}}
+        onSignInLink={goToSignIn}
+      />
     </div>
   );
 }
