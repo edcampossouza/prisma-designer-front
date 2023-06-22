f931618a020971cb1e4b034ebbb3f9f8a7396645
Eduardo Souza <edcampossouza@gmail.com>
2023-06-22 19:35:29 -0300

    feat: load saved schema

--- src/context/user.context.tsx
+++ src/context/user.context.tsx
@@ -1,13 +1,16 @@
 import { createContext } from "react";
 import { UserInfo } from "@/app/util/auth";
+import { Schema } from "prismadesign-lib";
 
 type UserContextType = {
   user?: UserInfo;
   setUser: React.Dispatch<React.SetStateAction<UserInfo | undefined>>;
+  setSchema: React.Dispatch<React.SetStateAction<Schema>>;
   notifyError: (error: any) => void;
 };
 export const UserContext = createContext<UserContextType>({
   user: undefined,
   setUser: () => {},
+  setSchema: () => {},
   notifyError: (error) => console.log(error),
 });
