a235395549dee1ed02aab1a40a9ae05e0bc01abd
Eduardo Souza <edcampossouza@gmail.com>
2023-06-28 22:18:33 -0300

    feat: support optional fields

--- src/app/components/prisma-objects/DataField.tsx
+++ src/app/components/prisma-objects/DataField.tsx
@@ -1,4 +1,4 @@
-import { Field } from "prismadesign-lib";
+import { Field, OptionalFieldAttribute } from "prismadesign-lib";
 import { AiOutlineCloseCircle } from "react-icons/ai";
 
 export default function DataField(props: {
@@ -9,8 +9,13 @@ export default function DataField(props: {
   return (
     <tr className="text-sm font-mono" id={props.xarrow_id}>
       <td className="pr-1 text-field-color">{props.field.name}</td>
-      <td className="px-1 text-type-color">{props.field.type.name}</td>
-      <td className="pl-1 text-attribute-color">{attributesToString(props.field)}</td>
+      <td className="px-1 text-type-color">
+        {props.field.type.name}
+        {props.field.attributes.includes(OptionalFieldAttribute) && "?"}
+      </td>
+      <td className="pl-1 text-attribute-color">
+        {attributesToString(props.field)}
+      </td>
       <td>
         <AiOutlineCloseCircle
           className="text-red-500 text-lg hover:cursor-pointer"
@@ -26,6 +31,7 @@ export default function DataField(props: {
 function attributesToString(field: Field): string {
   return field.attributes
     .map((att) => {
+      if (att.name === "optional") return;
       if (att.name !== "default") return `@${att.name}`;
       else return `@${att.name}(${field.defaultValue})`;
     })
